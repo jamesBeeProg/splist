448c1adccdd3ec33ba196980af324bdfbf1aecf1
James Clark <JamesClark@student.avrtech.com>
2020-01-30 14:15:33 -0500

    Add type-graphql and apollo to server

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,27 @@
-console.log('wew');
+import 'reflect-metadata';
+import http from 'http';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
 
-(async () => {})();
+(async () => {
+    const app = express();
+
+    const schema = await buildSchema({
+        resolvers: [__dirname + '/**/*.res.ts'],
+        emitSchemaFile: __dirname + '/../schema.gql',
+    });
+
+    const server = new ApolloServer({
+        playground: true,
+        schema,
+    });
+
+    server.applyMiddleware({ app });
+    const httpServer = http.createServer(app);
+    server.installSubscriptionHandlers(httpServer);
+
+    httpServer.listen(8080, () => {
+        console.log('----- Server started -----');
+    });
+})();
