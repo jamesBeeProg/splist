850a44bd33c968f08fca496e1eb503d16da99051
James Clark <JamesClark@student.avrtech.com>
2020-01-30 13:58:01 -0500

    Setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    rules: {
+        '@typescript-eslint/explicit-function-return-type': 0,
+    },
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+    ],
+    plugins: ['prettier'],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
