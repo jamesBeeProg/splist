e83fe0c704c14df339cd6c03d606bafe1288cf41
jamesBeeProg <jamesBeeProg@gmail.com>
2020-02-11 14:11:45 -0500

    Add typed-css-modules to have the .css typed corectly

---
+++ client/App.css.d.ts
@@ -0,0 +1,12 @@
+declare const styles: {
+    readonly app: string;
+    readonly 'branch-header': string;
+    readonly 'general-header': string;
+    readonly navbar: string;
+    readonly 'channel-list': string;
+    readonly main: string;
+    readonly 'member-list': string;
+    readonly toolbar: string;
+    readonly footer: string;
+};
+export = styles;
