41dc567877d7333dc9c8a48a0753b4a1d03a2a35
jamesBeeProg <jamesBeeProg@gmail.com>
2020-02-11 14:08:24 -0500

    Start basic layout and do the css grid/theming

--- client/App.tsx
+++ client/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { MessageView } from './message/MessageView';
 import { MessageInput } from './message/MessageInput';
+import { bind } from 'classnames/bind';
+import styles from './App.css';
+
+const cx = bind(styles);
 
 export const App = () => (
-    <div>
-        <MessageInput />
-        <MessageView />
+    <div className={cx('app')}>
+        <header className={cx('branch-header')}></header>
+        <header className={cx('general-header')}></header>
+        <nav className={cx('navbar')}></nav>
+        <aside className={cx('channel-list')}></aside>
+        <main className={cx('main')}>
+            <MessageView />
+        </main>
+        <aside className={cx('member-list')}></aside>
+        <footer className={cx('toolbar')}></footer>
+        <footer className={cx('footer')}>
+            <MessageInput />
+        </footer>
     </div>
 );
