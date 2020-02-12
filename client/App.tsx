b7ecb5bd06ad0313b70d572e5d9dcd2237d16824
jamesBeeProg <jamesBeeProg@gmail.com>
2020-02-12 12:54:32 -0500

    Use styled-components insted

--- client/App.tsx
+++ client/App.tsx
@@ -1,24 +1,87 @@
 import React from 'react';
 import { MessageView } from './message/MessageView';
 import { MessageInput } from './message/MessageInput';
-import { bind } from 'classnames/bind';
-import styles from './App.css';
+import styled, { createGlobalStyle } from 'styled-components';
 
-const cx = bind(styles);
+const Theme = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: Arial;
+
+        /* Customise theme here */
+        --main-background: #d9b382;
+        --aside-background: #363333;
+        --toolbar-background: #272121;
+        --navbar-background: #b55121;
+    }
+`;
+
+const Grid = styled.div`
+    /* Force clamp to window size */
+    width: 100vw;
+    height: 100vh;
+
+    display: grid;
+    grid:
+        ' navbar branch-header general-header general-header' 1fr
+        ' navbar channel-list  main           member-list   ' 10fr
+        ' navbar toolbar       footer         member-list   ' 1fr
+        / 1fr 3fr 8fr 3fr;
+`;
+
+const LodgeHeader = styled.header`
+    grid-area: branch-header;
+    background-color: var(--aside-background);
+    box-shadow: 0 5px 50px -10px rgba(0, 0, 0, 1);
+    z-index: 2;
+`;
+const ChannelHeader = styled.header`
+    grid-area: general-header;
+    background-color: var(--main-background);
+    box-shadow: 0 5px 50px -10px rgba(0, 0, 0, 1);
+    z-index: 1;
+`;
+const Navbar = styled.nav`
+    grid-area: navbar;
+    background-color: var(--navbar-background);
+    z-index: 3;
+`;
+const ChannelList = styled.aside`
+    grid-area: channel-list;
+    background-color: var(--aside-background);
+`;
+const Main = styled.main`
+    grid-area: main;
+    background-color: var(--main-background);
+`;
+const MemberList = styled.aside`
+    grid-area: member-list;
+    background-color: var(--aside-background);
+`;
+const Toolbar = styled.footer`
+    grid-area: toolbar;
+    background-color: var(--toolbar-background);
+`;
+const Footer = styled.footer`
+    grid-area: footer;
+    background-color: var(--main-background);
+`;
 
 export const App = () => (
-    <div className={cx('app')}>
-        <header className={cx('branch-header')}></header>
-        <header className={cx('general-header')}></header>
-        <nav className={cx('navbar')}></nav>
-        <aside className={cx('channel-list')}></aside>
-        <main className={cx('main')}>
+    <Grid>
+        <Theme />
+        <LodgeHeader></LodgeHeader>
+        <ChannelHeader></ChannelHeader>
+        <Navbar></Navbar>
+        <ChannelList></ChannelList>
+        <Main>
             <MessageView />
-        </main>
-        <aside className={cx('member-list')}></aside>
-        <footer className={cx('toolbar')}></footer>
-        <footer className={cx('footer')}>
+        </Main>
+        <MemberList></MemberList>
+        <Toolbar></Toolbar>
+        <Footer>
             <MessageInput />
-        </footer>
-    </div>
+        </Footer>
+    </Grid>
 );
